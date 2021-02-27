990483ec2d007b5136d3f73dba64646f195b48f1
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-02-27 11:16:37 +0300

    ödev 24şubat 01

---
+++ Entities/Concrete/CarImage.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class CarImage : IEntity
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public string ImagePath { get; set; }
+
+
+        private DateTime dateNow = DateTime.Today;
+
+        public DateTime Date
+        {
+            get { return dateNow; }
+            set { dateNow = value; }
+        }
+
+    }
+}
---
+++ SQLQuery4.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE CarImages(
+Id int primary key identity(1,1),
+CarId int,
+ImagePath nvarchar(37),
+Date datetime,
+foreign key(CarId) References Cars(CarId),
+)
+
+alter table CarImages
+alter column Date datetime2;
